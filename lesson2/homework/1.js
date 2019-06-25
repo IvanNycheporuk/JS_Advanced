82e655c39af621633c626f97d6c7dfc6610505ac
Ivan Nycheporuk <ivan.nycheporuk@gmail.com>
2019-06-25 10:28:01 +0300

    homework leeson2

--- lesson2/homework/1.js
+++ lesson2/homework/1.js
@@ -38,3 +38,32 @@
       }
 
   */
+
+var buttons = document.querySelectorAll('.showButton');
+
+var tabsArray = Array.from(document.querySelectorAll('.tab'));
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', function(e){
+    buttons.forEach(function(btn){
+      btn.classList.remove('active-btn');
+    })
+    e.target.classList.add('active-btn');
+    hideAllTabs();
+    getTabByDataTab(e.target.dataset.tab).classList.add('active');
+  })
+})
+
+function getTabByDataTab(tabId) {
+  return tabsArray.find(function(tab) {
+    return tab.dataset.tab == tabId
+  })
+}
+
+function hideAllTabs(){
+  tabsArray.forEach(function(tab){
+    tab.classList.remove('active');
+  })
+}
+
+
